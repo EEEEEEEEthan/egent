86ebee627309ca2d0a36a2e3551b6168a1019ce2
ethan <ethan.tang@coconutislandstudio.com>
2026-07-15 10:21:55 +0800

    Enhance start.bat to support upgrade option for dev dependencies
    Modified the start.bat script to allow installation of development dependencies when the --upgrade flag is provided. This change improves flexibility for users who want to ensure their environment is up-to-date before running the example agent.

--- start.bat
+++ start.bat
@@ -1,5 +1,7 @@
 @echo off
 cd /d "%~dp0"
-python -m pip install -e ".[dev]"
-if errorlevel 1 exit /b 1
+if "%~1"=="--upgrade" (
+    python -m pip install -e ".[dev]"
+    if errorlevel 1 exit /b 1
+)
 python examples/example_agent.py
